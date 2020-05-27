5038f7e65331620a6715b6ff8cadbfb865b4ffeb
Ankush Sood <aks21117@gmail.com>
2020-05-27 22:45:07 +0530

    EXCEL GENERATOR FOR LARAVEL USING LARAVEL MAATWEBSITE EXCEL PACKAGE. FIRST COMMIT.

---
+++ src/ExcelGeneratorServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ankush\Excelgenerator;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class ExcelGeneratorServiceProvider extends ServiceProvider
+{
+    public function boot(){
+
+    }
+
+    public function register(){
+
+    }
+}
